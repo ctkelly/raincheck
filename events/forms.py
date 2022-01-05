8dd7f5abe0b0c8af42adbb08607df95408062bc9
Chloe Kelly <chloetay3@gmail.com>
2022-01-04 19:36:33 -0500

    Add comments for date and time widgets

--- events/forms.py
+++ events/forms.py
@@ -17,24 +17,27 @@ class InvitationForm(ModelForm):
         exclude = ['invitee', 'event', 'response']
 
 
+class EventCreateForm(ModelForm):
+    class Meta:
+        model = Event
+        fields = ['title', 'invitee', 'date', 'time']
+        widgets = {
+            'date': DateInput(),  # OK -- model does not allow past dates
+            'time': TimeInput(),  # OK -- time appears as 00:00 and can select AM/PM
+        }
+
+
 class EventUpdateForm(ModelForm):
     class Meta:
         model = Event
         fields = ['title', 'date', 'time']
         widgets = {
             'date': DateInput(),
-            'time': TimeInput(),
-            # 'time': TimeInput(format='%H:%M')
+            # 'time': TimeInput(),  # Makes time in update form appear as 00:00:00 instead of 00:00 (AM/PM still ok)
+            'time': TimeInput(format='%H:%M'),
         }
 
 
-class EventCreateForm(ModelForm):
-    class Meta:
-        model = Event
-        fields = ['title', 'invitee', 'date', 'time']
-        widgets = {
-            'date': DateInput(),
-            'time': TimeInput(),
-        }
+
 
 
