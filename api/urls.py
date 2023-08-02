b7cf11d36e1d9e408a87d2a75f579cdeb3cac398
Chloe Kelly <chloetay3@gmail.com>
2023-08-02 18:07:48 -0400

    Update API view

--- api/urls.py
+++ api/urls.py
@@ -5,6 +5,5 @@ from api import views
 app_name = 'api'
 urlpatterns = [
     path('events/<int:pk>', views.EventDetailAPIView.as_view(), name='event_detail'),
-    path('events/', views.EventAPIView.as_view(), name='event_view')
 ]
 
--- api/views.py
+++ api/views.py
@@ -2,12 +2,9 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.permissions import IsAuthenticated
-import json
 
-from events.models import Event, Invitation
+from events.models import Event
 from events.serializers import EventSerializer
-from django.db.models import Q
-from datetime import date
 
 
 class EventDetailAPIView(APIView):
@@ -19,26 +16,4 @@ class EventDetailAPIView(APIView):
         event = Event.objects.get(pk=pk)
         serializer = EventSerializer(event)
         return Response(data=serializer.data, status=200)  # Need to pass data and status keyword args
-
-
-#  Trying something
-#  Need to set up a serializers.py to use the below? e.g. class EventSerializer(serializers.Serializer) etc...
-class EventAPIView(APIView):
-
-    authentication_classes = (SessionAuthentication,)  # A tuple
-    permission_classes = (IsAuthenticated,)
-
-    def get(self, request):
-        today = date.today()
-        events = Event.objects.filter(
-            Q(owner=self.request.user) |
-            Q(invitee=self.request.user),
-            date__gte=today
-        ).order_by('date')  # Leave in "order_by('date')" part for this view?
-
-        serializer = EventSerializer(events, many=True)
-        return Response({"events": serializer.data})
-
-
-
 
