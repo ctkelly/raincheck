40649de8acfd888973bbc2c03c2bad86beaa265e
Chloe Kelly <chloetay3@gmail.com>
2023-09-14 17:33:36 -0400

    Add cat fact API

--- home/views.py
+++ home/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
+import requests
 
 
 def index(request):
     template_name = 'home/main.html'
-    return render(request, template_name)
+    url = 'https://catfact.ninja/fact'
+    response = requests.get(url)
+    cat_fact = response.json()
+    ctx = {'fact': cat_fact['fact']}
+
+    return render(request, template_name, ctx)
